7683e4a28f068e6d1f40d597331e464e006aba15
Ben Lansdell <lansdell@uw.edu>
2018-10-23 15:56:16 -0400

    Rename things

---
+++ gym_buttons/__init__.py
@@ -0,0 +1,16 @@
+from gym.envs.registration import register
+
+register(
+    id='ButtonsFam-v0',
+    entry_point='gym_buttons.envs:ButtonsFamEnv'
+)
+
+register(
+    id='ButtonsObs-v0',
+    entry_point='gym_buttons.envs:ButtonsFamEnv'
+)
+
+register(
+    id='ButtonsTest-v0',
+    entry_point='gym_buttons.envs:ButtonsFamEnv'
+)
---
+++ gym_buttons/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_buttons.envs.button_env import ButtonEnv
