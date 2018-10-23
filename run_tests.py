239c12aee2b573087040a6939cd109214700acea
Ben Lansdell <lansdell@uw.edu>
2018-10-23 15:45:37 -0400

    Add test code

---
+++ run_tests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import random
+import numpy as np
+import gym
+
+# Test specifically importing a specific environment
+#from gym_minigrid.envs import DoorKeyEnv
+
+# Test importing wrappers
+#from gym_minigrid.wrappers import *
